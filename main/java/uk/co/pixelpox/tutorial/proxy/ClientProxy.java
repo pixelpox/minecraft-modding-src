3294c9bbdb909f552c435340391865c3adebc068
pixelpox <simon@pixelpox.co.uk>
2017-08-20 16:52:15 +0100

    Revert "finished step 2"
    This reverts commit 541d0242f9071c6a931265c987e1480e8c7983c1.

--- main/java/uk/co/pixelpox/tutorial/proxy/ClientProxy.java
+++
@@ -1,6 +0,0 @@
-package uk.co.pixelpox.tutorial.proxy;
-
-public class ClientProxy implements CommonProxy
-{
-
-}
--- main/java/uk/co/pixelpox/tutorial/proxy/ServerProxy.java
+++
@@ -1,6 +0,0 @@
-package uk.co.pixelpox.tutorial.proxy;
-
-public class ServerProxy implements CommonProxy
-{
-
-}
