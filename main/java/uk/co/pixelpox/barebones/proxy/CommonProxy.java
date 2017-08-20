1cbbec1d630e21aede576034b5ef95048e693883
pixelpox <simon@pixelpox.co.uk>
2017-08-20 17:07:43 +0100

    barebones setup

---
+++ main/java/uk/co/pixelpox/barebones/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package uk.co.pixelpox.barebones.proxy;
+
+public interface CommonProxy 
+{
+
+}
---
+++ main/java/uk/co/pixelpox/barebones/proxy/ServerProxy.java
@@ -0,0 +1,6 @@
+package uk.co.pixelpox.barebones.proxy;
+
+public class ServerProxy implements CommonProxy
+{
+
+}
